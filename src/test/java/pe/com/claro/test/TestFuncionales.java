20d82b5b96c4bef43b59ca812c11bf16b6e1b52b
DESKTOP-7KMN85N\admin <bestfrank2020@gmail.com>
2018-05-26 20:21:43 -0500

    modificar para test selenium

--- src/test/java/pe/com/claro/test/TestFuncionales.java
+++ src/test/java/pe/com/claro/test/TestFuncionales.java
@@ -3,33 +3,41 @@ package pe.com.claro.test;
 
 import com.thoughtworks.selenium.Selenium;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import com.thoughtworks.selenium.webdriven.WebDriverBackedSelenium;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+import java.net.URL;
 import java.util.regex.Pattern;
 import static org.apache.commons.lang3.StringUtils.join;
 
 public class TestFuncionales {
 	private Selenium selenium;
 	
-	@Test
-	public void CUcomparar() throws Exception {
-		// Do nothing
-	}
 
-/*	@Before
+	@Before
 	public void setUp() throws Exception {
-		WebDriver driver = new FirefoxDriver();
-		String baseUrl = "http://localhost:8080/DemoApp/index.jsp";
+		//agregado
+		String nodeURL="http://192.168.199.1:3333/wd/hub";
+		DesiredCapabilities capa= DesiredCapabilities.firefox();
+		capa.setBrowserName("chrome");
+		capa.setPlatform(Platform.WIN10);
+		WebDriver driver = new RemoteWebDriver(new URL(nodeURL),capa);
+		//retirado
+		//WebDriver driver = new FirefoxDriver();
+		String baseUrl = "http://host.docker.internal:8080/DemoApp/index.jsp";
 		selenium = new WebDriverBackedSelenium(driver, baseUrl);
 	}
 
 	@Test
 	public void CUabrirLink() throws Exception {
-		selenium.open("http://localhost:8080/DemoApp/index.jsp");
+		selenium.open("http://host.docker.internal:8080/DemoApp/index.jsp");
 		assertEquals("", selenium.getTitle());
 		selenium.click("link=Controlador get");
 	}
@@ -37,6 +45,6 @@ public class TestFuncionales {
 	@After
 	public void tearDown() throws Exception {
 		selenium.stop();
-	}*/
+	}
 }
 
